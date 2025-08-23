05408d5c7ade170d83141adc4786e20dbaf6d2df
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2025-08-23 17:23:38 +0330

    learn pass & query parameters

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hi vote")
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/greet/")   #pass parameter here
+async def read_root(name: str, age:Optional[int] = 50):  #query parameter here
+    return {"Hello": f"hi {name} you are {age}"}
+
