e2cd3f7217eae262099bc5ba4ef34757a651f7e0
mostafa-khodaparast <mostafa.khodaparast1997@gmail.com>
2025-09-11 13:50:27 +0330

    seperate crud logic using service class

--- books/schemas.py
+++ books/schemas.py
@@ -1,11 +1,32 @@
 from pydantic import BaseModel
+from datetime import datetime
+import uuid
 
 
 class Book(BaseModel):
-    id : int
+    id : uuid.UUID
     title: str
     author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str 
+    created_at: datetime
+    updated_at: datetime
 
 
-class BookUpdateModel:
-    pass
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
+
+
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
